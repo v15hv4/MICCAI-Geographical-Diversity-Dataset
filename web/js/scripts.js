5bb4a3b0b5e98e912e35ee27d9f1642829165e72
v15hv4 <vishva2912@gmail.com>
2020-10-06 22:03:01 +0530

    Add tab and view bars

---
+++ web/js/scripts.js
@@ -0,0 +1,15 @@
+function switchTab(year) {
+    var activeTab = document.getElementById("tabbar").getElementsByClassName("active")[0];
+    activeTab.classList.remove("active");
+    var newTab = document.getElementById("t" + year);
+    newTab.classList.add("active");
+    console.log(year);
+}
+
+function switchView(view) {
+    var activeView = document.getElementById("viewbar").getElementsByClassName("active")[0];
+    activeView.classList.remove("active");
+    var newView = document.getElementById("v" + view);
+    newView.classList.add("active");
+    console.log(view);
+}
